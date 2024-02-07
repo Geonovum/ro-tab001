6f71cd538ef441db5f0131b74f6352c4f48ba24a
Geonovum <>
2024-02-07 14:33:05 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/c369f46ef179716bd200fb4ed40f8a2e726fef91

---
+++ js/document-config.js
@@ -0,0 +1,31 @@
+var documentConfig =
+{
+  title: "Tabellen",
+  shortName: "tab001",
+  pubDomain: "ro",
+  specStatus: "def",
+  specType: "st",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/tab001/"
+  ],
+  publishDate: "2024-01-23",
+  thisVersion: [
+    "https://docs.geostandaarden.nl/ro/def-st-tab001-20240123/"
+  ],
+  edDraftURI: "https://geonovum.github.io/ro-tab001/",
+  authors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}
